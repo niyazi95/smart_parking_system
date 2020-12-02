f09670e507dc112fe6de36051f5d38fb05259b80
niyazi95 <49458443+niyazi95@users.noreply.github.com>
2020-12-02 09:55:17 +0300

    Update carparks.js

--- Server/routes/api/carparks.js
+++ Server/routes/api/carparks.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const bodyparser = require('body-parser') //Burası body-parser'ı kullanabilmek için
+
 const { admin } = require('../../firebaseConfig')
 const db = admin.firestore();
 
@@ -13,5 +15,18 @@ router.post('/', (req, res) => {
   })
 
 });
+
+//Burası bodyparser'a ayar vermek için
+router.use(bodyparser.urlencoded({extended:false})) 
+router.use(bodyparser.json()) 
+router.use(bodyparser.text({ type: 'text/*' })) // Bu satır olmadan plain text header'lı gönderim body-parser tarafından algılanmıyor.
+
+router.post('/raspberry', (req, res) => { //Burası Raspberry Pi'dan obje göndermek için
+    res.send("OK, Received!" + "\nPlate: " + req.body.plate)
+}); 
+router.post('/raspberry', (req, res) => {  //Burası Raspberry Pi'dan düz yazı göndermek için
+    res.send("OK, Received!" + "\nGelen veri:  " + req.body)
+}); 
+
 
 module.exports = router
